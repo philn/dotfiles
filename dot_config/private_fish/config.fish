86ecdd9c137297ae80eb1457ec1f8b377a0d67a0
Philippe Normand <philn@igalia.com>
2023-03-23 16:49:04 +0000

    fish: Ensure WAYLAND_DISPLAY is set when starting from BlackBox

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,11 @@ starship init fish | source
 
 if status is-interactive
 
+    # BlackBox doesn't set WAYLAND_DISPLAY, preventing pass -c to work.
+    if set -q TERM_PROGRAM
+        set -gx WAYLAND_DISPLAY "wayland-0"
+    end
+
     set -gx ATUIN_NOBIND "true"
     atuin init fish | source
     bind \cr _atuin_search
